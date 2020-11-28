e58dcdef3b507c45d0e79dc759f6fa0c8f625f59
LukeBattle <mail@lukebattle.com>
2020-11-28 23:11:05 +0000

    Added plot to clearly represent decline in viewers for each video

--- CSC8631 Project/src/first_analysis.R
+++ CSC8631 Project/src/first_analysis.R
@@ -1,7 +1,6 @@
 library(ProjectTemplate)
 load.project()
-
-
+library(dlookr)
 
 #plot graph, showing changing viewing percentage per run
 run_plot = ggplot(data = viewing_avg_long, aes(x = viewed, y = percentage, color = factor(Run))) + 
@@ -10,7 +9,7 @@ run_plot = ggplot(data = viewing_avg_long, aes(x = viewed, y = percentage, color
 
 run_plot
 
-run_plot2 = ggplot(data = viewing_avg_long, aes(x = Run, y = percentage, color = factor(viewed))) + 
+run_plot2 = ggplot(data = filter(viewing_avg_long,viewed ==95), aes(x = Run, y = percentage, color = factor(viewed))) + 
   geom_point() +
   geom_line()  
 
@@ -21,7 +20,10 @@ step_plot = ggplot(data = step_avg_long,aes(x = viewed, y = percentage, color = 
   geom_point() +
   geom_line()
 
+lost_viewer_plot = ggplot(data = step_avg_wide, aes(x = factor(step_position), y = percentage_drop_off)) + 
+  geom_col()
 
+lost_viewer_plot
 
 question_plot = ggplot(data = filter(correct_questions, Runs >= min(video_df$Run)), aes(x = Runs, y = percentage_correct)) +
   geom_point() + 
