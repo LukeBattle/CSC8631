1b472d02f346a85e58e9e9ad1a034dda5a2e4f34
LukeBattle <mail@lukebattle.com>
2020-12-03 10:27:43 +0000

    Introduced several more functions to rigorously check files

--- CSC8631 Project/lib/helpers.R
+++ CSC8631 Project/lib/helpers.R
@@ -9,14 +9,35 @@ check_empty = function (files) {
              
 }
   
-
-unique_videos = function (files) {
-  video_durations = numeric(13)
+check_dimensions = function (files, dimensions) {
+  dims_equal = logical(length(files))
   for (i in 1:length(files)) {
-    video_file = read.csv(paste("data/",files[i],sep = ""))
-    if (sum(video_durations) == 0) video_durations = unique(video_file$video_duration)
-    
-    
+    temp_file = read.csv(paste("data/",files[i],sep = ""))
+    dims_equal[i] = ifelse(all(dim(temp_file) == dimensions),TRUE,FALSE)
   }
+  return_string = ifelse(all(dims_equal), "All files have same dimensions", "Not all files have same dimensions")
+  return(return_string)
   
 }
+
+check_variables = function (files, variables) {
+  variables_equal = logical(length(files))
+  for (i in 1:length(files)) {
+    temp_file = read.csv(paste("data/",files[i],sep = ""))
+    variables_equal[i] = ifelse(all(colnames(temp_file)==variables),TRUE,FALSE)
+  }
+  return_string = ifelse(all(variables_equal), "All files have same variables", "Not all files have same variables")
+  return(return_string)
+}
+
+check_rows = function (files, rows, column_number){
+  rows_equal = logical(length(files))
+  for (i in 1:length(files)) {
+    temp_file = read.csv(paste("data/",files[i],sep = ""))
+    rows_equal[i] = ifelse(all(temp_file[,column_number]==rows),TRUE,FALSE)
+  }
+  return_string = ifelse(all(rows_equal), "All files have same rows", "Not all files have same rows")
+  return(return_string)
+}
+
+
--- CSC8631 Project/src/data_understanding.R
+++ CSC8631 Project/src/data_understanding.R
@@ -7,3 +7,12 @@ check_empty(dir("data",pattern = "weekly-sentiment-survey-responses"))
 check_empty(dir("data",pattern = "team-members"))
 check_empty(dir("data",pattern = "enrolments"))
 
+
+check_dimensions(dir("data",pattern = "video-stats"), dim(video_file_raw))
+check_dimensions(dir("data",pattern = "question-response"), dim(video_file_raw))
+
+check_variables(dir("data",pattern = "video-stats"), colnames(video_file_raw))
+
+check_rows(dir("data",pattern = "video-stats"),video_file_raw$title,2)
+
+check_rows(dir("data",pattern = "video-stats"),video_file_raw$step_position,1)
