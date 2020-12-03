1f13856f2a485c6c35ae9d0bb8318603d95ac352
LukeBattle <mail@lukebattle.com>
2020-12-03 09:05:09 +0000

    Create function that demonstrates lack of data in some files.

--- CSC8631 Project/lib/helpers.R
+++ CSC8631 Project/lib/helpers.R
@@ -1,4 +1,22 @@
-helper.function <- function()
-{
-  return(1)
+check_empty = function (files) {
+  empty_flag = logical(length(files))
+  for (i in 1:length(files)) {
+    temp_file = read.csv(paste("data/",files[i],sep = ""))
+    empty_flag[i] = ifelse(dim(temp_file)[1] == 0, TRUE, FALSE)
+  }
+  
+  return(paste(sum(as.integer(empty_flag)), "out of",length(files), "files are empty"))                     
+             
+}
+  
+
+unique_videos = function (files) {
+  video_durations = numeric(13)
+  for (i in 1:length(files)) {
+    video_file = read.csv(paste("data/",files[i],sep = ""))
+    if (sum(video_durations) == 0) video_durations = unique(video_file$video_duration)
+    
+    
+  }
+  
 }
---
+++ CSC8631 Project/src/data_understanding.R
@@ -0,0 +1,9 @@
+
+check_empty(dir("data",pattern = "archetype-survey-responses"))
+check_empty(dir("data",pattern = "video-stats"))
+check_empty(dir("data",pattern = "question-response"))
+check_empty(dir("data",pattern = "leaving-survey-responses"))
+check_empty(dir("data",pattern = "weekly-sentiment-survey-responses"))
+check_empty(dir("data",pattern = "team-members"))
+check_empty(dir("data",pattern = "enrolments"))
+
